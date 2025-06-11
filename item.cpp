f6eee06d6994f0c4379d3b23f16ba6597b1121b5
ali hosseini <alihosseinihf63@gmail.com>
2025-06-11 12:41:10 +0330

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,11 +1,14 @@
 #include "item.hpp"
+#include "location.hpp"
+#include <random>
 #include <algorithm>
-#include <vector>
+#include <stdexcept>
+#include <chrono>
 
-Item::Item(string name, ItemColor color, int power, const std::string& location) :name(name), color(color), power(power), location(location) {}
+using namespace std;
 
-string Item::getname() const{
-    return name;
+Item::Item(string itemName, ItemColor color, int power, const shared_ptr<Location> location) : itemName(itemName), color(color), power(power) {
+    setLocation(location);
 }
 
 ItemColor Item::getColor() const {
@@ -16,120 +19,156 @@ int Item::getPower() const {
     return power;
 }
 
-string Item::getLocation() const {
+string Item::getItemName() const {
+    return itemName;
+}
+
+shared_ptr<Location> Item::getLocation() const {
     return location;
 }
 
-void Item::setLocation(const string& newLocation) {
-    location = newLocation;
+void Item::setLocation(shared_ptr<Location> location) {
+    this->location = location;
 }
 
 string Item::colorToString(ItemColor color) {
     switch (color) {
-        case ItemColor::Red: return "Red";
-        case ItemColor::Blue: return "Blue";
-        case ItemColor::Yellow: return "Yellow";
-        default: return "Unknown";
+        case ItemColor::Red:
+            return "Red";
+        case ItemColor::Blue:
+            return "Blue";
+        case ItemColor::Yellow:
+            return "Yellow";
+        default:
+            return "Unknown";
     }
 }
 
-ItemBag::ItemBag() {
+ItemBag::ItemBag(Map& map) {
     rng.seed(chrono::steady_clock::now().time_since_epoch().count());
 
-    // Red Items
-    items.emplace_back("Torch", ItemColor::Red, 5, "Barn");
-    items.emplace_back("Torch", ItemColor::Red, 5, "Barn");
-    items.emplace_back("Dart", ItemColor::Red, 2, "Inn");
-    items.emplace_back("Dart", ItemColor::Red, 2, "Inn");
-    items.emplace_back("Fire pocker", ItemColor::Red, 3, "Mansion");
-    items.emplace_back("Fire pocker", ItemColor::Red, 3, "Mansion");
-    items.emplace_back("Rapier", ItemColor::Red, 5, "Theatre");
-    items.emplace_back("Rapier", ItemColor::Red, 5, "Theatre");
-    items.emplace_back("Shovel", ItemColor::Red, 2, "Graveyard");
-    items.emplace_back("Shovel", ItemColor::Red, 2, "Graveyard");
-    items.emplace_back("Pitchfork", ItemColor::Red, 4, "Barn");
-    items.emplace_back("Pitchfork", ItemColor::Red, 4, "Barn");
-    items.emplace_back("Rifle", ItemColor::Red, 6, "Barn");
-    items.emplace_back("Rifle", ItemColor::Red, 6, "Barn");
-    items.emplace_back("Silver cane", ItemColor::Red, 6, "Hsop");
-    items.emplace_back("Silver cane", ItemColor::Red, 6, "Hsop");
-    items.emplace_back("Knife", ItemColor::Red, 3, "Docks");
-    items.emplace_back("Knife", ItemColor::Red, 3, "Docks");
-    items.emplace_back("Pistol", ItemColor::Red, 6, "Precict");
-    items.emplace_back("Pistol", ItemColor::Red, 6, "Precinct");
-    items.emplace_back("Bear trap", ItemColor::Red, 4, "Shop");
-    items.emplace_back("Bear trap", ItemColor::Red, 4, "Shop");
-    items.emplace_back("Speargun", ItemColor::Red, 4, "Institute");
-    items.emplace_back("Speargun", ItemColor::Red, 4, "Institute");
+    items.emplace_back("Torch", ItemColor::Red, 5, map.getLocation("Barn"));
+    items.emplace_back("Torch", ItemColor::Red, 5, map.getLocation("Barn"));
+    items.emplace_back("Dart", ItemColor::Red, 2, map.getLocation("Inn"));
+    items.emplace_back("Dart", ItemColor::Red, 2, map.getLocation("Inn"));
+    items.emplace_back("Fire pocker", ItemColor::Red, 3, map.getLocation("Mansion"));
+    items.emplace_back("Fire pocker", ItemColor::Red, 3, map.getLocation("Mansion"));
+    items.emplace_back("Rapier", ItemColor::Red, 5, map.getLocation("Theatre"));
+    items.emplace_back("Rapier", ItemColor::Red, 5, map.getLocation("Theatre"));
+    items.emplace_back("Shovel", ItemColor::Red, 2, map.getLocation("Graveyard"));
+    items.emplace_back("Shovel", ItemColor::Red, 2, map.getLocation("Graveyard"));
+    items.emplace_back("Pitchfork", ItemColor::Red, 4, map.getLocation("Barn"));
+    items.emplace_back("Pitchfork", ItemColor::Red, 4, map.getLocation("Barn"));
+    items.emplace_back("Rifle", ItemColor::Red, 6, map.getLocation("Barn"));
+    items.emplace_back("Rifle", ItemColor::Red, 6, map.getLocation("Barn"));
+    items.emplace_back("Silver cane", ItemColor::Red, 6, map.getLocation("Shop"));
+    items.emplace_back("Silver cane", ItemColor::Red, 6, map.getLocation("Shop"));
+    items.emplace_back("Knife", ItemColor::Red, 3, map.getLocation("Docks"));
+    items.emplace_back("Knife", ItemColor::Red, 3, map.getLocation("Docks"));
+    items.emplace_back("Pistol", ItemColor::Red, 6, map.getLocation("Precinct"));
+    items.emplace_back("Pistol", ItemColor::Red, 6, map.getLocation("Precinct"));
+    items.emplace_back("Bear trap", ItemColor::Red, 4, map.getLocation("Shop"));
+    items.emplace_back("Bear trap", ItemColor::Red, 4, map.getLocation("Shop"));
+    items.emplace_back("Speargun", ItemColor::Red, 4, map.getLocation("Institute"));
+    items.emplace_back("Speargun", ItemColor::Red, 4, map.getLocation("Institute"));
 
+    items.emplace_back("Anatomy test", ItemColor::Blue, 1, map.getLocation("Institute"));
+    items.emplace_back("Anatomy test", ItemColor::Blue, 1, map.getLocation("Institute"));
+    items.emplace_back("Centrifuge", ItemColor::Blue, 1, map.getLocation("Laboratory")); 
+    items.emplace_back("Centrifuge", ItemColor::Blue, 1, map.getLocation("Laboratory"));
+    items.emplace_back("Kite", ItemColor::Blue, 1, map.getLocation("Tower"));
+    items.emplace_back("Kite", ItemColor::Blue, 1, map.getLocation("Tower"));
+    items.emplace_back("Research", ItemColor::Blue, 2, map.getLocation("Tower"));
+    items.emplace_back("Research", ItemColor::Blue, 2, map.getLocation("Tower"));
+    items.emplace_back("Telescope", ItemColor::Blue, 2, map.getLocation("Mansion"));
+    items.emplace_back("Telescope", ItemColor::Blue, 2, map.getLocation("Mansion"));
+    items.emplace_back("Searchlight", ItemColor::Blue, 2, map.getLocation("Precinct"));
+    items.emplace_back("Searchlight", ItemColor::Blue, 2, map.getLocation("Precinct"));
+    items.emplace_back("Experiment", ItemColor::Blue, 2,map.getLocation("Laboratory"));
+    items.emplace_back("Experiment", ItemColor::Blue, 2, map.getLocation("Laboratory"));
+    items.emplace_back("Analysis", ItemColor::Blue, 2, map.getLocation("Institute"));
+    items.emplace_back("Analysis", ItemColor::Blue, 2, map.getLocation("Institute"));
+    items.emplace_back("Rotenone", ItemColor::Blue, 3, map.getLocation("Institute"));
+    items.emplace_back("Rotenone", ItemColor::Blue, 3, map.getLocation("Institute"));
+    items.emplace_back("Cosmic Ray Diffuser", ItemColor::Blue, 3, map.getLocation("Tower"));
+    items.emplace_back("Cosmic Ray Diffuser", ItemColor::Blue, 3, map.getLocation("Tower"));
+    items.emplace_back("Nebularium", ItemColor::Blue, 3, map.getLocation("Tower"));
+    items.emplace_back("Nebularium", ItemColor::Blue, 3, map.getLocation("Tower"));
+    items.emplace_back("Fossil", ItemColor::Blue, 3, map.getLocation("Camp"));
+    items.emplace_back("Fossil", ItemColor::Blue, 3, map.getLocation("Camp"));
+    items.emplace_back("Monocane Mixture", ItemColor::Blue, 3, map.getLocation("Inn"));
+    items.emplace_back("Monocane Mixture", ItemColor::Blue, 3, map.getLocation("Inn"));
 
-    // Blue Items
-    items.emplace_back("Anatomy test", ItemColor::Blue, 1, "Institute");
-    items.emplace_back("Anatomy test", ItemColor::Blue, 1, "Institute");
-    items.emplace_back("Centrifuge", ItemColor::Blue, 1, "Laboratory"); 
-    items.emplace_back("Centrifuge", ItemColor::Blue, 1, "Laboratory");
-    items.emplace_back("Kite", ItemColor::Blue, 1, "Tower");
-    items.emplace_back("Kite", ItemColor::Blue, 1, "Tower");
-    items.emplace_back("Research", ItemColor::Blue, 2, "Tower");
-    items.emplace_back("Research", ItemColor::Blue, 2, "Tower");
-    items.emplace_back("Telescope", ItemColor::Blue, 2, "Mansion");
-    items.emplace_back("Telescope", ItemColor::Blue, 2, "Mansion");
-    items.emplace_back("Searchlight", ItemColor::Blue, 2, "precint");
-    items.emplace_back("Searchlight", ItemColor::Blue, 2, "precint");
-    items.emplace_back("Experiment", ItemColor::Blue, 2, "Laboratory");
-    items.emplace_back("Experiment", ItemColor::Blue, 2, "Laboratory");
-    items.emplace_back("Analysis", ItemColor::Blue, 2, "Institute");
-    items.emplace_back("Analysis", ItemColor::Blue, 2, "Institute");
-    items.emplace_back("Rotenone", ItemColor::Blue, 3, "Institute");
-    items.emplace_back("Rotenone", ItemColor::Blue, 3, "Institute");
-    items.emplace_back("Cosmic Ray Diffuser", ItemColor::Blue, 3, "Tower");
-    items.emplace_back("Cosmic Ray Diffuser", ItemColor::Blue, 3, "Tower");
-    items.emplace_back("Nebularium", ItemColor::Blue, 3, "Tower");
-    items.emplace_back("Nebularium", ItemColor::Blue, 3, "Tower");
-    items.emplace_back("Fossil", ItemColor::Blue, 3, "camp");
-    items.emplace_back("Fossil", ItemColor::Blue, 3, "camp");
-    items.emplace_back("Monocane Mixture", ItemColor::Blue, 3, "Inn");
-    items.emplace_back("Monocane Mixture", ItemColor::Blue, 3, "Inn");
+    items.emplace_back("Flower", ItemColor::Yellow, 2, map.getLocation("Docks"));
+    items.emplace_back("Flower", ItemColor::Yellow, 2, map.getLocation("Docks"));
+    items.emplace_back("Tarot dech", ItemColor::Yellow, 3, map.getLocation("Camp"));
+    items.emplace_back("Tarot dech", ItemColor::Yellow, 3, map.getLocation("Camp"));
+    items.emplace_back("Garlic", ItemColor::Yellow, 2, map.getLocation("Inn"));
+    items.emplace_back("Garlic", ItemColor::Yellow, 2, map.getLocation("Inn"));
+    items.emplace_back("Mirrored Box", ItemColor::Yellow, 3, map.getLocation("Mansion"));
+    items.emplace_back("Mirrored Box", ItemColor::Yellow, 3, map.getLocation("Mansion"));
+    items.emplace_back("Stake", ItemColor::Yellow, 3, map.getLocation("Abbey"));
+    items.emplace_back("Stake", ItemColor::Yellow, 3, map.getLocation("Abbey"));
+    items.emplace_back("Scroll of Thoth", ItemColor::Yellow, 4, map.getLocation("Museum"));
+    items.emplace_back("Scroll of Thoth", ItemColor::Yellow, 4, map.getLocation("Museum"));
+    items.emplace_back("violin", ItemColor::Yellow, 3, map.getLocation("Camp"));
+    items.emplace_back("violin", ItemColor::Yellow, 3, map.getLocation("Camp"));
+    items.emplace_back("tablet", ItemColor::Yellow, 3, map.getLocation("Mansion"));
+    items.emplace_back("tablet", ItemColor::Yellow, 3, map.getLocation("Mansion"));
+    items.emplace_back("Wolfsbane", ItemColor::Yellow, 4, map.getLocation("Camp"));
+    items.emplace_back("Wolfsbane", ItemColor::Yellow, 4, map.getLocation("Camp"));
+    items.emplace_back("Charm", ItemColor::Yellow, 4, map.getLocation("Camp"));
+    items.emplace_back("Charm", ItemColor::Yellow, 4, map.getLocation("Camp"));
 
-    // Yellow Items
-
-    items.emplace_back("Flower", ItemColor::Yellow, 2, "Docks");
-    items.emplace_back("Flower", ItemColor::Yellow, 2, "Docks");
-    items.emplace_back("Tarot dech", ItemColor::Yellow, 3, "Camp");
-    items.emplace_back("Tarot dech", ItemColor::Yellow, 3, "Camp");
-    items.emplace_back("Garlic", ItemColor::Yellow, 2, "Inn");
-    items.emplace_back("Garlic", ItemColor::Yellow, 2, "Inn");
-    items.emplace_back("Mirrored Box", ItemColor::Yellow, 3, "Mansion");
-    items.emplace_back("Mirrored Box", ItemColor::Yellow, 3, "Mansion");
-    items.emplace_back("Stake", ItemColor::Yellow, 3, "Abbey");
-    items.emplace_back("Stake", ItemColor::Yellow, 3, "Abbey");
-    items.emplace_back("Scroll of Thoth", ItemColor::Yellow, 4, "Museum");
-    items.emplace_back("Scroll of Thoth", ItemColor::Yellow, 4, "Museum");
-    items.emplace_back("violin", ItemColor::Yellow, 3, "Camp");
-    items.emplace_back("violin", ItemColor::Yellow, 3, "Camp");
-    items.emplace_back("tablet", ItemColor::Yellow, 3, "Museum");
-    items.emplace_back("tablet", ItemColor::Yellow, 3, "Museum");
-    items.emplace_back("Wolfsbane", ItemColor::Yellow, 4, "Camp");
-    items.emplace_back("Wolfsbane", ItemColor::Yellow, 4, "Camp");
-    items.emplace_back("Charm", ItemColor::Yellow, 4, "Camp");
-    items.emplace_back("Charm", ItemColor::Yellow, 4, "Camp");
-
+    initialItems = items;
+    
     shuffleItems();
+
+    for (int i = 0; i < 12; ++i) {
+        drawRandomItem(map);
+    }
 }
 
 void ItemBag::shuffleItems() {
     shuffle(items.begin(), items.end(), rng);
 }
 
-bool ItemBag::isEmpty() const {
-    return items.empty();
+void ItemBag::refillItems(Map& map) {
+    vector<Item> usedItems;
+    for (const auto& pair : map.locations) {
+        const auto& location = pair.second;
+        const auto& locItems = location->getItems();
+        usedItems.insert(usedItems.end(), locItems.begin(), locItems.end());
+    }
+
+    items.clear();
+
+    for (const auto& item : initialItems) {
+        auto it = find_if(usedItems.begin(), usedItems.end(), [&](const Item& used) {
+            return item.getItemName() == used.getItemName() &&
+                   item.getColor() == used.getColor() &&
+                   item.getPower() == used.getPower() &&
+                   item.getLocation() == used.getLocation();
+        });
+
+        if (it == usedItems.end()) {
+            items.push_back(item);
+        }
+    }
+
+    shuffleItems();
 }
 
-Item ItemBag::drawRandomItem() {
+
+Item ItemBag::drawRandomItem(Map& map) {
     if (items.empty()) {
-        throw runtime_error("Item bag is empty!");
+        refillItems(map);
     }
-    Item top = items.back();
+    shuffleItems();
+
+    Item item = items.back();
     items.pop_back();
-    return top;
+
+    auto location = map.getLocation(item.getLocation()->getName());
+    location->addItem(item);
 }

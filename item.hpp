95a6cd6a570fe1f677be1c2bb7b72fbbb42e1401
ali hosseini <alihosseinihf63@gmail.com>
2025-06-12 23:49:19 +0330

    Update item.hpp
    item had changed and i forgot to send it

--- item.hpp
+++ item.hpp
@@ -13,36 +13,46 @@ enum class ItemColor {
     Yellow
 };
 
+class Location; 
+
 class Item {
 private:
-    string name;
+    std::string itemName;
     ItemColor color;   
     int power;  
-    string location;
-
+    std::shared_ptr<Location> location;
 public:
-    Item(string name, ItemColor color, int power, const string& location);
-    string getname() const;
+    Item(std::string itemName, ItemColor color, int power, std::shared_ptr<Location> location);
+
     ItemColor getColor() const;
     int getPower() const;
-    string getLocation() const;
+    std::string getItemName() const;
 
-    void setLocation(const string& newLocation);
+    std::shared_ptr<Location> getLocation() const;
+    void setLocation(std::shared_ptr<Location> location);
 
     static string colorToString(ItemColor color);
 };
 
+class Map;  
+
 class ItemBag {
+public:
+    explicit ItemBag(Map& map);
+
+    void shuffleItems();
+    void refillItems(Map& map);
+    Item drawRandomItem(Map& map);
+    const vector<Item>& getItems() const;
+
 private:
     vector<Item> items;
-    mt19937 rng;
-
-public:
-    ItemBag();       
-    void shuffleItems();  
-    bool isEmpty() const;
-    Item drawRandomItem();      
+    vector<Item> initialItems;
+    mt19937 rng{random_device{}()};
 };
+
+
+#endif
 
 
 #endif

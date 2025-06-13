feb63d90ff00932df9dc12787523c4ded2118e71
Ehsan2c <Ehsan.2c84@gmail.com>
2025-06-13 12:17:21 +0330

    Town was changed to Location

--- map.cpp
+++ map.cpp
@@ -1,49 +1,50 @@
 #include "map.hpp"
+#include "location.hpp"
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 Map::Map() {
-    shared_ptr<Town> Cave = make_shared<Town>("Cave");
-    shared_ptr<Town> Camp = make_shared<Town>("Camp");
-    shared_ptr<Town> Precinct = make_shared<Town>("Precinct");
-    shared_ptr<Town> Inn = make_shared<Town>("Inn");
-    shared_ptr<Town> Barn = make_shared<Town>("Barn");
-    shared_ptr<Town> Dungeon = make_shared<Town>("Dungeon");
-    shared_ptr<Town> Theatre = make_shared<Town>("Theatre");
-    shared_ptr<Town> Tower = make_shared<Town>("Tower");
-    shared_ptr<Town> Docks = make_shared<Town>("Docks");
-    shared_ptr<Town> Mansion = make_shared<Town>("Mansion");
-    shared_ptr<Town> Abbey = make_shared<Town>("Abbey");
-    shared_ptr<Town> Shop = make_shared<Town>("Shop");
-    shared_ptr<Town> Crypt = make_shared<Town>("Crypt");
-    shared_ptr<Town> Museum = make_shared<Town>("Museum");
-    shared_ptr<Town> Church = make_shared<Town>("Church");
-    shared_ptr<Town> Laboratory = make_shared<Town>("Laboratory");
-    shared_ptr<Town> Hospital = make_shared<Town>("Hospital");
-    shared_ptr<Town> Graveyard = make_shared<Town>("Graveyard");
-    shared_ptr<Town> Institute = make_shared<Town>("Institute");
+    shared_ptr<Location> Cave = make_shared<Location>("Cave");
+    shared_ptr<Location> Camp = make_shared<Location>("Camp");
+    shared_ptr<Location> Precinct = make_shared<Location>("Precinct");
+    shared_ptr<Location> Inn = make_shared<Location>("Inn");
+    shared_ptr<Location> Barn = make_shared<Location>("Barn");
+    shared_ptr<Location> Dungeon = make_shared<Location>("Dungeon");
+    shared_ptr<Location> Theatre = make_shared<Location>("Theatre");
+    shared_ptr<Location> Tower = make_shared<Location>("Tower");
+    shared_ptr<Location> Docks = make_shared<Location>("Docks");
+    shared_ptr<Location> Mansion = make_shared<Location>("Mansion");
+    shared_ptr<Location> Abbey = make_shared<Location>("Abbey");
+    shared_ptr<Location> Shop = make_shared<Location>("Shop");
+    shared_ptr<Location> Crypt = make_shared<Location>("Crypt");
+    shared_ptr<Location> Museum = make_shared<Location>("Museum");
+    shared_ptr<Location> Church = make_shared<Location>("Church");
+    shared_ptr<Location> Laboratory = make_shared<Location>("Laboratory");
+    shared_ptr<Location> Hospital = make_shared<Location>("Hospital");
+    shared_ptr<Location> Graveyard = make_shared<Location>("Graveyard");
+    shared_ptr<Location> Institute = make_shared<Location>("Institute");
 
-    addTown(Cave);
-    addTown(Camp);
-    addTown(Precinct);
-    addTown(Inn);
-    addTown(Barn);
-    addTown(Dungeon);
-    addTown(Theatre);
-    addTown(Tower);
-    addTown(Docks);
-    addTown(Mansion);
-    addTown(Abbey);
-    addTown(Shop);
-    addTown(Crypt);
-    addTown(Museum);
-    addTown(Church);
-    addTown(Laboratory);
-    addTown(Hospital);
-    addTown(Graveyard);
-    addTown(Institute);
+    addLocation(Cave);
+    addLocation(Camp);
+    addLocation(Precinct);
+    addLocation(Inn);
+    addLocation(Barn);
+    addLocation(Dungeon);
+    addLocation(Theatre);
+    addLocation(Tower);
+    addLocation(Docks);
+    addLocation(Mansion);
+    addLocation(Abbey);
+    addLocation(Shop);
+    addLocation(Crypt);
+    addLocation(Museum);
+    addLocation(Church);
+    addLocation(Laboratory);
+    addLocation(Hospital);
+    addLocation(Graveyard);
+    addLocation(Institute);
 
     addNeighbor("Cave", "Camp");
     addNeighbor("Camp", "Precinct");
@@ -73,35 +74,35 @@ Map::Map() {
     addNeighbor("Laboratory", "Institute"); 
 }
 
-void Map::addTown(shared_ptr<Town> town) {
-    if (towns.find(town->getName()) != towns.end()) {
-            throw invalid_argument("Town '" + town->getName() + "' already exists in map.");
+void Map::addLocation(shared_ptr<Location> location) {
+    if (locations.find(location->getName()) != locations.end()) {
+            throw invalid_argument("Location '" + location->getName() + "' already exists in map.");
     }
-    if (town) {
-        towns[town->getName()] = town;
+    if (location) {
+        locations[location->getName()] = location;
     } 
     else {
-        throw invalid_argument("Invalid town.");
+        throw invalid_argument("Invalid location.");
     }
 }
 
-void Map::addNeighbor(const string& townName1, const string& townName2) {
-    auto it1 = towns.find(townName1);
-    auto it2 = towns.find(townName2);
+void Map::addNeighbor(const string& locationName1, const string& locationName2) {
+    auto it1 = locations.find(locationName1);
+    auto it2 = locations.find(locationName2);
 
-    if (it1 != towns.end() && it2 != towns.end()) {
+    if (it1 != locations.end() && it2 != locations.end()) {
         it1->second->addNeighbor(it2->second);
         it2->second->addNeighbor(it1->second);
     } 
     else {
-        throw invalid_argument("Couldn't find one or both towns to set as neighbors.");
+        throw invalid_argument("Couldn't find one or both locations to set as neighbors.");
     }
 }
 
-shared_ptr<Town> Map::getTown(const std::string& townName) const {
-    auto it = towns.find(townName);
-    if (it != towns.end()) {
+shared_ptr<Location> Map::getLocation(const std::string& locationName) const {
+    auto it = locations.find(locationName);
+    if (it != locations.end()) {
         return it->second;
     }
-    throw invalid_argument(townName + " doesn't exist.");
+    throw invalid_argument(locationName + " doesn't exist.");
 }

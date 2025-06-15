40b30e45afcfcdea02e1a498b8b42a7db7e94e47
Ehsan2c <Ehsan.2c84@gmail.com>
2025-06-15 16:30:36 +0330

    Implementation of power function and power name

--- dracula.cpp
+++ dracula.cpp
@@ -1,5 +1,18 @@
 #include "dracula.hpp"
+#include "hero.hpp"
 
 using namespace std;
 
-Dracula::Dracula(shared_ptr<Town> startingTown) : Monster("Dracula", startingTown) {}
+Dracula::Dracula(shared_ptr<Location> startingLocation) : Monster("Dracula", startingLocation) {
+    powerName = "Dark Charm";
+}
+
+void Dracula::power(Hero* hero) {
+    if (currentLocation == hero->getCurrentLocation()) return;
+
+    auto heroLocation = hero->getCurrentLocation();
+    hero->setCurrentLocation(currentLocation);
+    heroLocation->removeCharacter(hero->getHeroName());
+    currentLocation->addCharacter(hero->getHeroName());
+    cout << hero->getPlayerName() << " (" << hero->getHeroName() << ") moved to " << hero->getCurrentLocation()->getName() << ".\n";
+}

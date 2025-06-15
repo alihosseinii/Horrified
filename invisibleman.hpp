2c7aa5f35356135f2851a98999f5c4b7b72c2435
Ehsan2c <Ehsan.2c84@gmail.com>
2025-06-15 16:29:04 +0330

    prototype of power function and power name

--- invisibleman.hpp
+++ invisibleman.hpp
@@ -1,12 +1,20 @@
 #ifndef INVISIBLEMAN_HPP
 #define INVISIBLEMAN_HPP
 
+#include <string>
+#include <memory>
 #include "monster.hpp"
 
-class InvisibleMan : public Monster {
-public:
-    InvisibleMan(std::shared_ptr<Town> startingTown);
+class Location;
 
+class InvisibleMan : public Monster {
+private:
+std::string powerName;
+
+public:
+    InvisibleMan(std::shared_ptr<Location> startingLocation);
+
+    void power(Hero* hero) override;
 };
 
 #endif

9d154951165e2b74e0b373897d83f29542ed870f
ali hosseini <alihosseinihf63@gmail.com>
2025-06-29 01:00:07 +0330

    Update frenzymarker.cpp

--- frenzymarker.cpp
+++ frenzymarker.cpp
@@ -1,29 +1,33 @@
 #include "frenzymarker.hpp"
 #include <algorithm>
 
-FrenzyMarker::FrenzyMarker(const std::vector<Monster*>& allMonsters)
-    : monsterOrder(allMonsters) {
-    currentFrenzied = !monsterOrder.empty() ? monsterOrder[0] : nullptr;
+class Monster;
+FrenzyMarker::FrenzyMarker(Monster* dracula, Monster* invisibleMan) {
+    monsterOrder.push_back(dracula);
+    monsterOrder.push_back(invisibleMan);
+    currentFrenzied = dracula;
 }
 
 Monster* FrenzyMarker::getCurrentFrenzied() const {
     return currentFrenzied;
 }
+void FrenzyMarker::advance(Monster* dracula, Monster* invisibleMan) {
+    if (dracula->getCurrentLocation() == nullptr){
+        monsterOrder.at(1) = nullptr;
+        currentFrenzied = dracula;
+        return;
+    }
 
-void FrenzyMarker::advance() {
     if (monsterOrder.empty() || currentFrenzied == nullptr) return;
 
     auto it = std::find(monsterOrder.begin(), monsterOrder.end(), currentFrenzied);
     if (it != monsterOrder.end()) {
         ++it;
         if (it == monsterOrder.end()) {
-            currentFrenzied = monsterOrder[0];
+            currentFrenzied = monsterOrder[0]; 
         } else {
             currentFrenzied = *it;
         }
     }
 }
 
-void FrenzyMarker::reset() {
-    currentFrenzied = !monsterOrder.empty() ? monsterOrder[0] : nullptr;
-}

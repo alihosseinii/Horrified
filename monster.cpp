4527ce5edea85b277898f291d14fabefe7963572
Ehsan2c <Ehsan.2c84@gmail.com>
2025-06-29 20:06:18 +0330

    implementation of attack function

--- monster.cpp
+++ monster.cpp
@@ -1,4 +1,8 @@
 #include "monster.hpp"
+#include "terrorteracker.hpp"
+#include "map.hpp"
+#include "archeologist.hpp"
+#include "mayor.hpp"
 #include <iostream>
 #include <queue>
 #include <unordered_map>
@@ -94,6 +98,97 @@ void Monster::moveToNearestCharacter(const string& targetCharacter, int stepNumb
     }
 }
 
+bool Monster::attack(Hero* archeologist, Hero* mayor, TerrorTracker& terrorTracker, Map& map) {
+    auto currentLocationCharacters = currentLocation->getCharacters();
+    Hero* targetHero = nullptr;
+    string targetVillager = "";
+    
+    for (const auto& character : currentLocationCharacters) {
+        if (character == "Archeologist") {
+            targetHero = archeologist;
+            break;
+        } else if (character == "Mayor") {
+            targetHero = mayor;
+            break;
+        }
+    }
+    
+    if (!targetHero) {
+        for (const auto& character : currentLocationCharacters) {
+            if (character != "Invisible man" && character != "Dracula" && 
+                character != "Archeologist" && character != "Mayor") {
+                targetVillager = character;
+                break;
+            }
+        }
+    }
+    
+    if (!targetHero && targetVillager.empty()) {
+        return false;
+    }
+    
+    cout << monsterName << " attacks ";
+    
+    if (targetHero) {
+        cout << targetHero->getPlayerName() << " (" << targetHero->getHeroName() << ")!\n";
+        
+        string answer;
+        while (true) {
+            cout << "Do you want to use an item to defend yourself? (Yes/No): ";
+            getline(cin, answer);
+            if (answer == "Yes" || answer == "yes" || answer == "No" || answer == "no") break;
+            cout << "Invalid input. Please enter Yes or No.\n";
+        }
+        
+        if (answer == "Yes" || answer == "yes") {
+            auto items = targetHero->getItems();
+            if (!items.empty()) {
+                cout << "Available items:\n";
+                for (size_t i = 0; i < items.size(); ++i) {
+                    cout << i + 1 << ". " << items[i].getItemName() << " (" 
+                         << Item::colorToString(items[i].getColor()) << ", Power: " 
+                         << items[i].getPower() << ")\n";
+                }
+                
+                cout << "Enter item number to use (0 to cancel): ";
+                int choice;
+                cin >> choice;
+                cin.ignore();
+                
+                if (choice > 0 && choice <= static_cast<int>(items.size())) {
+                    cout << targetHero->getPlayerName() << " used " << items[choice-1].getItemName() << " to defend!\n";
+                    
+                    targetHero->removeItem(choice - 1);
+                    
+                    return true;
+                }
+            } else {
+                cout << "You have no items to use!\n";
+            }
+        }
+        
+        try {
+            auto hospital = map.getLocation("Hospital");
+            currentLocation->removeCharacter(targetHero->getHeroName());
+            hospital->addCharacter(targetHero->getHeroName());
+            targetHero->setCurrentLocation(hospital);
+            cout << targetHero->getPlayerName() << " (" << targetHero->getHeroName() << ") was wounded and moved to Hospital.\n";
 
+            terrorTracker.increase();
+            cout << "Terror level increased to " << terrorTracker.getLevel() << " due to the attack!\n";
+            return true;
+        } catch (const exception& e) {
+            cout << e.what() << endl;
+        }
+        
+    } else if (!targetVillager.empty()) {
+        cout << targetVillager << "!\n";
+        currentLocation->removeCharacter(targetVillager);
+        cout << targetVillager << " was killed by " << monsterName << "!\n";
 
-
+        terrorTracker.increase();
+        cout << "Terror level increased to " << terrorTracker.getLevel() << " due to the attack!\n";
+        
+        return true;
+    }
+}

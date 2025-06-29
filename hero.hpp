d74df2ad1aa6b91f96948dab1a3799d871db171c
Ehsan2c <Ehsan.2c84@gmail.com>
2025-06-29 20:24:29 +0330

    prototype of move and guide function was modified and a vector was added for perk cards and prototype of addPerkCard and getPerkcards and diaplayPerkCards was added

--- hero.hpp
+++ hero.hpp
@@ -8,6 +8,9 @@
 #include "map.hpp"
 #include "villagermanager.hpp"
 #include "item.hpp"
+#include "perkcard.hpp"
+
+class PerkDeck;
 
 std::string toSentenceCase(std::string name);
 
@@ -16,8 +19,8 @@ public:
     Hero(const std::string& playerName, const std::string& heroName, int maxActions, std::shared_ptr<Location> startingLocation);
     virtual ~Hero() = default;
 
-    virtual void move(std::shared_ptr<Location> newLocation, VillagerManager& villagerManager);
-    virtual void guide(VillagerManager& villagerManager, Map& map);
+    virtual void move(std::shared_ptr<Location> newLocation, VillagerManager& villagerManager, PerkDeck* perkDeck = nullptr);
+    virtual void guide(VillagerManager& villagerManager, Map& map, PerkDeck* perkDeck = nullptr);
     virtual void pickUp();
     // virtual void advance();
     // virtual void defeat();
@@ -31,15 +34,18 @@ public:
     void resetActions(); 
 
     std::vector<Item> getItems() const;
+    void removeItem(size_t index);
 
-    // void addPerkCard(const PerkCard& card);
-    // const std::vector<PerkCard>& getPerkCards() const;
+    void addPerkCard(const PerkCard& card);
+    const std::vector<PerkCard>& getPerkCards() const;
+    void displayPerkCards() const;
 
     std::shared_ptr<Location> getCurrentLocation() const;
     void setCurrentLocation(std::shared_ptr<Location> currentLocation);
     
 protected:
     std::vector<Item> items;
+    std::vector<PerkCard> perkCards;
     std::shared_ptr<Location> currentLocation;
     std::string heroName;
     std::string playerName;

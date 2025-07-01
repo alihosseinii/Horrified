3bc5337a7e84f31214098cd011b3434296a07947
ali hosseini <alihosseinihf63@gmail.com>
2025-07-01 11:35:57 +0330

    Update TaskBoard.hpp
    Destruction of Dracula and Invisible man is complete

--- TaskBoard.hpp
+++ TaskBoard.hpp
@@ -1,34 +1,44 @@
-#ifndef TASKBOARD_H
-#define TASKBOARD_H
+#ifndef TASKBOARD_HPP
+#define TASKBOARD_HPP
 
 #include <string>
 #include <unordered_map>
 
-using namespace std;
-
-enum class MonsterType {
-    Dracula,
-    InvisibleMan
+struct TaskStatus {
+    int currentStrength = 0;
+    bool completed = false;
 };
 
 class TaskBoard {
 private:
-    unordered_map<string, bool> draculaCoffins;  
-    unordered_map<string, bool> invisibleManEvidence; 
+    std::unordered_map<std::string, TaskStatus> draculaCoffins;  
+    std::unordered_map<std::string, bool> invisibleManCluesDelivered;
+    TaskStatus draculaDefeat;
+    TaskStatus invisibleManDefeat;
+    bool invisibleManDefeated = false;
 
 public:
     TaskBoard();
 
-    void destroyCoffinAt(const string& location);
-    bool isCoffinDestroyed(const string& location) const;
+    void addStrengthToCoffin(const std::string& location, int strength);
+    bool isCoffinLocation(const std::string& location) const;
+    bool isCoffinDestroyed(const std::string& location) const;
     bool allCoffinsDestroyed() const;
 
-    void placeEvidenceFrom(const string& location);
-    bool isEvidencePlaced(const string& location) const;
-    bool allEvidencePlaced() const;
+    void addStrengthToDracula(int strength);
+    int getDraculaDefeatStrength() const;
+    bool isDraculaDefeated() const;
 
-    string getDraculaTaskStatus() const;
-    string getInvisibleManTaskStatus() const;
+    bool isClueDelivered(const std::string& location) const;
+    void deliverClue(const std::string& location);
+    bool allCluesDelivered() const;
+    void addStrengthToInvisibleMan(int strength);
+    int getInvisibleManDefeatStrength() const;
+    void defeatInvisibleMan();
+    bool isInvisibleManDefeated() const;
+
+    std::string getDraculaTaskStatus() const;
+    std::string getInvisibleManClueStatus() const;
 };
 
 #endif

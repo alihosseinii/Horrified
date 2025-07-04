da0fe827959cefaaecfbb0aebc603edef8052932
ali hosseini <alihosseinihf63@gmail.com>
2025-07-04 20:10:20 +0330

    Update frenzymarker.hpp

--- frenzymarker.hpp
+++ frenzymarker.hpp
@@ -1,18 +1,19 @@
 #ifndef FRENZYMARKER_HPP
 #define FRENZYMARKER_HPP
 
+#include "monster.hpp"
 #include <vector>
-#include "Monster.hpp"
 
 class FrenzyMarker {
 private:
     Monster* currentFrenzied;
-    vector<Monster*> monsterOrder;
+    std::vector<Monster*> monsterOrder;
 
 public:
     FrenzyMarker(Monster* dracula, Monster* invisibleMan);
 
     Monster* getCurrentFrenzied() const;
-    void advance(Monster* dracula, Monster* invisibleMan); 
+    void advance(Monster* dracula, Monster* invisibleMan);
 };
+
 #endif

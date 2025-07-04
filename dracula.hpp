43996804d54d1bbb3003d984556f68f76de1a41b
Ehsan2c <Ehsan.2c84@gmail.com>
2025-07-04 17:37:49 +0330

    Prototype of power was modified

--- dracula.hpp
+++ dracula.hpp
@@ -3,14 +3,16 @@
 
 #include "monster.hpp"
 
+class TerrorTracker;
+
 class Dracula : public Monster {
 private:
-std::string powerName;
+    std::string powerName;
 
 public:
     Dracula(std::shared_ptr<Location> startingLocation);
 
-    void power(Hero* hero) override;
+    void power(Hero* hero, TerrorTracker& terrorTracker) override;
 };
 
 #endif

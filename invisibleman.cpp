6a38a5075ca9aa4122ca906a556639243c9bfea4
Ehsan2c <Ehsan.2c84@gmail.com>
2025-07-04 21:08:19 +0330

    Implementation of power was modified

--- invisibleman.cpp
+++ invisibleman.cpp
@@ -25,6 +25,7 @@ void InvisibleMan::power(Hero* hero, TerrorTracker& terrorTracker) {
             return;
         }
     }
+    return;
 }
 
 void InvisibleMan::moveTowardsVillager(int steps) {
@@ -80,8 +81,8 @@ void InvisibleMan::moveTowardsVillager(int steps) {
     int moveCount = min(steps, static_cast<int>(path.size()));
     for (int i = 0; i < moveCount; ++i) {
         currentLocation->removeCharacter(monsterName);
-        path[i]->addCharacter(monsterName);
         setCurrentLocation(path[i]);
+        path[i]->addCharacter(monsterName);
         cout << monsterName << " moved to " << path[i]->getName() << ".\n";
 
         auto newLocationCharacters = path[i]->getCharacters();
